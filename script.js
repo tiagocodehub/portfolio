6e866037911a265a047865e8d73a3f783732f093
Tiago Santos <tiago.tss1988@gmail.com>
2024-11-10 21:52:52 -0300

    modo white ativo

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleTheme() {
+    // Alterna entre o modo escuro e claro
+    document.body.classList.toggle('white-mode');
+}
+
+function changeLanguage(language) {
+    // Seleciona todos os elementos que têm atributos de idioma
+    const elements = document.querySelectorAll('[data-lang-en], [data-lang-pt]');
+
+    elements.forEach(element => {
+        // Altera o texto do elemento de acordo com o idioma selecionado
+        if (language === 'en') {
+            element.textContent = element.getAttribute('data-lang-en');
+        } else {
+            element.textContent = element.getAttribute('data-lang-pt');
+        }
+    });
+}
